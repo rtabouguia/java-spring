1c75325b9eb8cf01cf8f443c6df2459d0ccf99e0
TABOUGUIA <tabouguiaraissa@yahoo.fr>
2022-08-29 15:35:16 +0200

    execution with profile dev or prod configuration

--- javaspring/src/main/java/fr/m2i/javaspring/JavaspringApplication.java
+++ javaspring/src/main/java/fr/m2i/javaspring/JavaspringApplication.java
@@ -4,14 +4,23 @@ import fr.m2i.javaspring.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.*;
+
 
 @SpringBootApplication
 public class JavaspringApplication {
 
           public static void main(String[] args) {
-	ApplicationContext ctx = SpringApplication.run(JavaspringApplication.class, args);
-                  DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
-                  dataConfig.setup();
-	}
+              ConfigurableEnvironment env = new StandardEnvironment();
+                env.setActiveProfiles("prod");
 
+                SpringApplication sa = new SpringApplication(JavaspringApplication.class);
+                sa.setEnvironment(env);
+
+                ApplicationContext ctx = sa.run(args);
+
+                DataSourceConfig dataConfig = ctx.getBean(DataSourceConfig.class);
+                dataConfig.setup();
+
+    }
 }
